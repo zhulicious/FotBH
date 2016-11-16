8767a21f4c606c94e64c3a6e6f2dc78f7e0340ba
Stenkrona <axelstenkrona@me.com>
2016-11-16 18:34:21 +0100

    AudioTracks Added!
    -PuddleFootSteps x 10
    -CaveFootSteps x 8
    -ForestBackGroundTrack
    -CaveBackGroundTrack

---
+++ BH4/Assets/Scripts/AudioScripts/Audio_Mama.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Mama : MonoBehaviour {
+
+    private AudioSource mainBackGroundTrack;
+    private AudioSource owlHoots;
+
+    public List<AudioClip> puddleFootSteps;
+    public List<AudioClip> caveFootStep;
+
+    public AudioClip caveBackGroundTrack;
+    public AudioClip forestBackGroundTrack;
+    public AudioClip owlHootTrack;
+
+    
+    void Start()
+    {
+        mainBackGroundTrack = transform.GetChild(0).transform.GetChild(0).GetComponent<AudioSource>();
+        owlHoots = transform.GetChild(0).transform.GetChild(1).GetComponent<AudioSource>();
+    }
+
+    public void PlayCaveBackGroundTrack()
+    {
+        mainBackGroundTrack.clip = caveBackGroundTrack;
+    }
+    public void PlayForestBackGroundTrack()
+    {
+        mainBackGroundTrack.clip = forestBackGroundTrack;
+    }
+    
+}
