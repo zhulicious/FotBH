94f81062d66603528cf0cbc27d94c838641689d1
Azzi <azul_zulis@hotmail.com>
2016-11-23 16:42:08 +0100

    Hopefully fixed menu

---
+++ BH4/Assets/MainMenu/Scripts/PCE_buttonScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine; using UnityEngine.SceneManagement;
+using System.Collections;
+
+/*
+P C E = Play, Credits, Exit
+
+*/
+
+public class PCE_buttonScript : MonoBehaviour {
+
+    public MenuScript menuScript;
+    public TuvaMenuScript tvms;
+	
+    public void ActivateMe ()
+    {
+        if (currentName == "Play")
+        {
+            //SceneManager.LoadScene("trollskog_vs_1");
+            Debug.Log("LoadScene(trollskog)");
+        }
+        else if (currentName == "Credits")
+        {
+            tvms.StopWalkingTuva();
+            menuScript.ChangeToCreditPanel("Credits");
+        }
+        else if (currentName == "Exit")
+        {
+            //Application.Quit();
+            Debug.Log("Application.Quit();");
+        }
+        else if (currentName == "Return")
+        {
+
+            tvms.StartCoroutine("GetTuvaMoving");
+            menuScript.ChangeToCreditPanel("Menu");
+        }
+    }
+
+    string currentName 
+    {
+        get
+        {
+            return this.gameObject.name;
+        }
+    }
+}
---
+++ BH4/Assets/MainMenu/Scripts/TuvaMenuScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+*       This script will only be usedd for Tuva in the Main Menu.
+*
+*/
+
+public class TuvaMenuScript : MonoBehaviour {
+
+    Transform tuva;
+    float movingSpeed;
+    Vector3 tuvaStartPosition;
+
+    public Vector3[] positions;
+    public int positionsIndexer;
+    bool movingCharacter = false;
+
+    TuvaButtonScript tbs;
+
+    void Start ()
+    {
+        tuva = GameObject.Find("Tuva").transform;
+        tuvaStartPosition = tuva.transform.position;
+        tbs = GetComponent<TuvaButtonScript>();
+        movingSpeed = 1.35f;
+        StartCoroutine("GetTuvaMoving");
+
+    }
+
+    void Update ()
+    {
+        if (movingCharacter)
+        {
+            tuva.position = Vector3.MoveTowards(tuva.position, positions[positionsIndexer], movingSpeed * Time.deltaTime);
+        }
+    }
+
+    IEnumerator GetTuvaMoving()
+
+    {
+        yield return new WaitForSeconds(1.0f);
+        movingCharacter = true;
+        tbs.DisplayKeyButtons(positionsIndexer);
+        yield return new WaitUntil(() => tuva.position == positions[positionsIndexer]);
+        movingCharacter = false;
+        positionsIndexer++;
+        yield return new WaitForSeconds(2.0f);
+        movingCharacter = true;
+        tbs.DisplayKeyButtons(positionsIndexer);
+        yield return new WaitUntil(() => tuva.position == positions[positionsIndexer]);
+        movingCharacter = false;
+        positionsIndexer++;
+        yield return new WaitForSeconds(2.0f);
+        movingCharacter = true;
+       /* tbs.DisplayKeyButtons(positionsIndexer);*/ Debug.Log("Activate E-button");
+        yield return new WaitUntil(() => tuva.position == positions[positionsIndexer]);
+        movingCharacter = false;
+        yield return new WaitUntil(() => tuva.position == positions[positionsIndexer]);
+        yield break;
+
+    }
+
+    public void StopWalkingTuva()
+    {
+        StopCoroutine("GetTuvaMoving");
+
+        movingCharacter = false;
+        tuva.transform.position = tuvaStartPosition;
+    }
+}
