4df8c0f23db9f46c99e2c30da292cbe43fa15c7d
Azzi <azul_zulis@hotmail.com>
2016-12-01 16:20:10 +0100

    MERGE LEVEL!!!!!!!

---
+++ BH4/Assets/Graphics/Characters/Treeman/StoppChase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoppChase : MonoBehaviour {
+
+    public bool sChase;
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.name == "Enemy")
+        {
+            sChase = true;
+            Debug.Log(sChase + " " + "triggerd");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+}
