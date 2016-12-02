f6480511074ce9b42ee41a91a06d94280a6d1244
Azzi <azul_zulis@hotmail.com>
2016-12-02 16:38:23 +0100

    fixed treeman

---
+++ BH4/Assets/Scripts/SafeZones.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZones : MonoBehaviour {
+
+    public bool tuvaIsHere;
+
+    void Start ()
+    {
+        tuvaIsHere = false;
+    }
+
+    void OnTriggerStay2D (Collider2D col)
+    {
+        if (col.gameObject.name == "Tuva")
+        {
+            Debug.Log("Tuva has entered");
+            tuvaIsHere = true;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D col)
+    {
+        if (col.gameObject.name == "Tuva")
+        {
+            Debug.Log("Tuva has exited");
+            tuvaIsHere = false;
+        }
+    }
+}
